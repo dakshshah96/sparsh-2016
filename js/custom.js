600ad76170dbe3da48fe3b122d3e106edb6f9881
Daksh Shah <dakshshah96@gmail.com>
2015-12-24 13:26:48 +0300

    Merged Maps.js with custom.js.

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,3 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
@@ -28,3 +22,27 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// Google Maps
+
+var myCenter=new google.maps.LatLng(21.163825, 72.785476);  //Lat,Long of SVNIT
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:14,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };														//Map Properties
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  animation:google.maps.Animation.BOUNCE,
+  });
+
+marker.setMap(map);
+}															//Map Marker,properties
+
+google.maps.event.addDomListener(window, 'load', initialize);
